e760e05784c799cf6163fd287d5b5a1424305e93
Emmanuel Egomson <fegomson@gmail.com>
2022-01-26 16:43:42 -0600

    Connect the backend to the frontend and fetch posts from the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState(1);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(`/api/v1/posts?page=${page}`);
+        const { data, pages: totalPages } = await res.json();
+        setPages(totalPages);
+        setPosts(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+        setError("Something went wrong");
+      }
+    };
+
+    fetchPosts();
+  }, [page]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Pagination Component */}
+      {/* Posts Listing */}
+      {/* Pagination Component */}
     </div>
   );
-}
+};
 
 export default App;
